31d25a07950a710010983a6b0c70b592f2c8f9b5
reedspool <reedmagic@gmail.com>
2015-05-04 16:15:33 -0400

    Add MIT License

--- d3.transition.styleWithEase.js
+++ d3.transition.styleWithEase.js
@@ -7,8 +7,29 @@
 /* See the [github repository](https://github.com/Graphicacy/d3_multidimensional_easing)
 /*
 /* Author, [Reed](https://github.com/reedspool)
-/*          
-/* Made By [Graphicacy](http://www.graphicacy.com)
+/*
+/* License:
+/* The MIT License (MIT)
+/* 
+/* Copyright (c) [2015] [Graphicacy](http://www.graphicacy.com)
+/* 
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
+/* of this software and associated documentation files (the "Software"), to deal
+/* in the Software without restriction, including without limitation the rights
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/* copies of the Software, and to permit persons to whom the Software is
+/* furnished to do so, subject to the following conditions:
+/* 
+/* The above copyright notice and this permission notice shall be included in all
+/* copies or substantial portions of the Software.
+/* 
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/* SOFTWARE.
 /*
 /*- -~- -*/
 d3.transition.prototype.styleWithEase = 
